8c3e98e89630339cdab5e52b039b0afdbc91377c
golonzovsky <alex.golonzovsky@gmail.com>
2017-03-13 09:05:25 +0100

    spring cloud version update, simplify app, gitlab-ci draft

--- README.md
+++ README.md
@@ -3,11 +3,16 @@ Dummy project for deployment investigations.
 
 #### rough list of possible todo:
 
+- [ ] maven release plugin
 - [ ] scm plugin
+
 - [ ] marathon
-- [ ] docker 
+
+- [ ] docker
+- [ ] docker repository
+
 - [ ] gitlab ci
-- [ ] consider using something bigger e.g. [vehicle fleet](https://github.com/golonzovsky/vehicle-fleet-demo)
+- [ ] consider using something bigger e.g. [vehicle fleet](https://github.com/golonzovsky/vehicle-fleet-demo) or [bootiful microservices](https://github.com/joshlong/bootiful-microservices)
 
 #### links
 [tomcat-undeploy-all](https://gist.github.com/golonzovsky/ae9cb92c020f0b8833519447b7471281)   
--- src/main/java/com/golonzovsky/DeploymeApplication.java
+++ src/main/java/com/golonzovsky/DeploymeApplication.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Data;
-
 @SpringBootApplication
 public class DeploymeApplication {
 
@@ -17,17 +15,9 @@ public class DeploymeApplication {
 }
 
 
-@RequestMapping("/")
 @RestController
-class DeploymeRestController {
+@RequestMapping("/")
+class HalloController {
 
-    @GetMapping
-    Message hello() {
-        return new Message("hallo");
-    }
-}
-
-@Data
-class Message {
-    final String text;
+    @GetMapping String hello() { return "hallo stranger"; }
 }
